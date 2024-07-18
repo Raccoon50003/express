8908e2345bc90d8d7f4c42d9e6bfb6699f86aa75
Shinji Kimura <2201150003vl@cyber-u.ac.jp>
2024-07-18 13:39:26 +0900

    update no.14

---
+++ routes/notes_from_b.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+//const uri = "mongodb+srv://matsunami2:password@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://2201150003vl:t1X2bvW2jU0fJ8v4@cluster0.8uyd8ut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
+
+res.json(note);
+})
+
+module.exports = router;
