b87bf39673eeebbca5969a9839cd1d549b6a7bcf
Shinji Kimura <2201150003vl@cyber-u.ac.jp>
2024-07-18 14:37:28 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
-
+require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-//const uri = "mongodb+srv://matsunami2:password@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb+srv://2201150003vl:t1X2bvW2jU0fJ8v4@cluster0.8uyd8ut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
